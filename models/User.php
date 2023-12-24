d7c3af0ee6694ef02bd241f66860ebf7c31b6b97
AllaouiJihad <allaouijihad05@gmail.com>
2023-12-24 17:12:46 +0100

    add method of signup and getALLApprenants

--- models/User.php
+++ models/User.php
@@ -2,7 +2,35 @@
 
     include '../core/database.php';
     class User{
-        
+        private $lastname;
+        private $firstname;
+        private $email;
+        private $phone;
+        private $address;
+        private $cin;
+        private $birthdate;
+        private $pwd;
+
+        public function getAllapprenants(){
+            $sql = "select * from utilisateur where id_role = 3";
+            $row= Database::connexion()->getPdo()->query($sql)->fetch(PDO::FETCH_OBJ);
+            return $row;
+        }
+        public function signup($lastname, $firstname, $email, $phone, $CIN, $birthdate, $password) {            
+            $sql = "INSERT INTO utilisateur (lastname, firstname, email, phone, CIN, birthdate, password, id_role) 
+                    VALUES ('$lastname','$firstname', '$email', '$phone','$CIN', '$birthdate', '$password', 3)";
+
+            $result = Database::connexion()->getPdo()->query($sql);
+
+            if ($result) {
+                echo "Record inserted successfully";
+            } 
+            else {
+                echo "Error: " . $sql . "<br>" . Database::connexion()->getPdo()->errorInfo()[2];
+            }
+        }
+    
+
         // public function getAllapprenants(){
         //     return $this->connexion()->query('select * from utilisateur where id_role = 3')->fetchAll(PDO::FETCH_OBJ);
         // }
